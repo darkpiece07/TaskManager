8850a7222e56f5f82bdf420323f5e7803adcf857
Ashish Singh <ashishsingh07.work@gmail.com>
2024-01-26 08:27:07 +0530

    API add_new_task developed

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -11,7 +12,7 @@ class Task(models.Model):
 
     title = models.CharField(max_length=50, blank=False, null = False, unique=True)
     description = models.TextField(blank=False, null=False)
-    due_date = models.DateField()
+    due_date = models.DateField(default = timezone.now)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
 
     def __str__(self):
--- app/views.py
+++ app/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Task
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def index(request):
     context = {}
     return render(request, 'app/index.html', context)
 
+
+# Create a task
+@csrf_exempt
+def addTask(request):
+    data = {'success' : False, 'message': "Your task is added to the all Tasks!"}
+    if request.method == "POST":
+        title = request.POST.get("title")
+        task = Task.objects.filter(title = title)
+        # checking for duplicate task!
+        if task.exists():
+            data = {'success' : False, 'message': "Task exists with the same name!"}
+            print("task exists in the database.")
+            return JsonResponse({"data": data})
+        
+        description = request.POST.get("description")
+        due_date = request.POST.get("due_date")
+        task = Task(title = title, description = description, due_date = due_date)
+        task.save()
+        data = {
+            "success": True,
+            "id" : task.id,
+            "title": task.title,
+            "description": task.description,
+            "due_date": task.due_date,
+            "status": task.status
+        }
+
+    return JsonResponse({"data": data})
 
